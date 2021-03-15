ecb31c05ca307bd6af55d099be76905b461e6c70
Alessandro Falcone <alessandro.falcone@ymail.com>
2021-03-15 17:39:11 +0100

    Aggiunta funzione mouseenter/mouseleave

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-$(document).ready(function() {
 
+$('#link-funzionalita').mouseenter(function() {
+  $('.funzionalita').toggle();
+});
+$('#link-funzionalita').mouseleave(function() {
+  $('.funzionalita').toggle();
+});
 
+$('#link-abbonamenti').mouseenter(function() {
+  $('.abbonamenti').toggle();
+});
+$('#link-abbonamenti').mouseleave(function() {
+  $('.abbonamenti').toggle();
+});
 
+$('#link-chi-siamo').mouseenter(function() {
+  $('.chi-siamo').toggle();
+});
+$('#link-chi-siamo').mouseleave(function() {
+  $('.chi-siamo').toggle();
+});
+
+$('#link-aiuto').mouseenter(function() {
+  $('.aiuto').toggle();
+});
+$('#link-aiuto').mouseleave(function() {
+  $('.aiuto').toggle();
 });
